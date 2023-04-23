66446b7c9252c188c4f79c33e5133604fe6f160a
newKidddInTown <vietduc.bk69@gmail.com>
2023-04-23 17:22:14 +0700

    OK status

--- registration/src/main/java/com/example/registration/rpc/RegistrationGrpcServer.java
+++ registration/src/main/java/com/example/registration/rpc/RegistrationGrpcServer.java
@@ -7,15 +7,19 @@ import com.example.registration.service.RegistrationService;
 import io.grpc.stub.StreamObserver;
 import lombok.RequiredArgsConstructor;
 import net.devh.boot.grpc.server.service.GrpcService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 
 @GrpcService
 @RequiredArgsConstructor
 public class RegistrationGrpcServer extends RegistrationGrpc.RegistrationImplBase{
-
+    private final Logger log = LoggerFactory.getLogger(RegistrationGrpcServer.class);
     private final RegistrationService registrationService;
     @Override
     public void login(LoginRequest request, StreamObserver<LoginResponse> responseObserver) {
 
+        log.info("Received request from web service : {}", request);
         // logics...
         String token = registrationService.getToken();
 
