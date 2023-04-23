79470db5deadab244670e2defbfd08c7481fb4aa
newKidddInTown <vietduc.bk69@gmail.com>
2023-04-24 00:23:28 +0700

    roll back grpc client

--- web/src/main/java/com/example/web/service/CalculatorService.java
+++ web/src/main/java/com/example/web/service/CalculatorService.java
@@ -2,9 +2,6 @@ package com.example.web.service;
 
 import com.example.autogen.grpc.calculator.SumGrpc;
 import com.example.autogen.grpc.calculator.SumRequest;
-import com.example.autogen.grpc.registration.RegistrationGrpc;
-import io.grpc.Channel;
-import io.grpc.ManagedChannelBuilder;
 import net.devh.boot.grpc.client.inject.GrpcClient;
 import org.springframework.stereotype.Service;
 
@@ -12,18 +9,13 @@ import java.util.List;
 
 @Service
 public class CalculatorService {
-//    @GrpcClient("calculator")
-//    private SumGrpc.SumBlockingStub client;
+    @GrpcClient("calculator")
+    private SumGrpc.SumBlockingStub client;
 
     public Integer getSum(List<Integer> numbers) {
-
-        Channel channel = ManagedChannelBuilder.forAddress("calculator", 9090).usePlaintext().build();
-        SumGrpc.SumBlockingStub stub
-                = SumGrpc.newBlockingStub(channel);
-
         SumRequest sumRequest = SumRequest.newBuilder()
                 .addAllNumbers(numbers)
                 .build();
-        return stub.getSum(sumRequest).getSum();
+        return client.getSum(sumRequest).getSum();
     }
 }
--- web/src/main/java/com/example/web/service/RegistrationService.java
+++ web/src/main/java/com/example/web/service/RegistrationService.java
@@ -4,19 +4,17 @@ import com.example.autogen.grpc.registration.LoginRequest;
 import com.example.autogen.grpc.registration.LoginResponse;
 import com.example.autogen.grpc.registration.RegistrationGrpc;
 import com.example.web.model.LoginReq;
-import io.grpc.Channel;
-import io.grpc.ManagedChannelBuilder;
+import net.devh.boot.grpc.client.inject.GrpcClient;
 import org.springframework.stereotype.Service;
 
 @Service
 public class RegistrationService {
+
+    @GrpcClient("registration")
+    private RegistrationGrpc.RegistrationBlockingStub client;
     public String authenticate(LoginReq request) {
 
-        Channel channel = ManagedChannelBuilder.forAddress("registration", 9092).usePlaintext().build();
-        RegistrationGrpc.RegistrationBlockingStub stub
-                = RegistrationGrpc.newBlockingStub(channel);
-
-        LoginResponse rpcResponse = stub.login(
+        LoginResponse rpcResponse = client.login(
                 LoginRequest.newBuilder()
                         .setUsername(request.getUsername())
                         .setPassword(request.getPassword())
